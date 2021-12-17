ba1708cb3a6f43454fef8ff6af7c0b6ca7b57f92
Anton Eliseev <anton.g.eliseev@gmail.com>
2021-12-17 23:52:59 +0300

    Add solution for Task3

---
+++ CodeAloneTinkoff2021/Solution/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Hello, World!");
+
+var fileName = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Resources\advent_3.test.txt");
+
+if (!File.Exists(fileName))
+{
+    Console.WriteLine("File not found. Path: " + fileName);
+    return;
+}
+
+var lines = File.ReadAllLines(fileName);
+
+var length = int.Parse(lines[0].Split(' ').Last());
+
+var pieces = lines[1].Split(' ').Select(x => int.Parse(x)).ToArray();
+pieces = pieces.OrderByDescending(x => x).ToArray();
+
+var sum = 0;
+var knots = -1;
+
+foreach (var piece in pieces)
+{
+    sum += piece;
+    knots++;
+
+    if (sum >= length) break;
+}
+
+Console.WriteLine(knots);

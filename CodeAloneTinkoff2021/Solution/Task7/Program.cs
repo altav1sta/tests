cf90ef673b3ad6e596a3c592dae2aa22c23382e2
Anton Eliseev <anton.g.eliseev@gmail.com>
2021-12-18 17:20:12 +0300

    Add solution of Task 7

---
+++ CodeAloneTinkoff2021/Solution/Task7/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Hello, World!");
+
+var fileName = Path.Combine(Environment.CurrentDirectory, @"..\..\..\Resources\advent_7.test.txt");
+
+if (!File.Exists(fileName))
+{
+    Console.WriteLine("File not found. Path: " + fileName);
+    return;
+}
+
+var lines = File.ReadAllLines(fileName);
+var cans = lines.Select(line =>
+    line
+    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    .Select(x => int.Parse(x))
+    .Skip(1)
+    .ToArray())
+    .ToArray();
+//var cans = new[]
+//{
+//    new int[] {  },
+//    new int[] { 2, 3 },
+//    new int[] { },
+//    new int[] {}
+//};
+var mostSticky = new HashSet<int>();
+Console.WriteLine();
+for (var i = 1; i < cans.Length; i++)
+{
+    if (cans[i].Length == 0) // we don't know what is more sticky than i
+    {
+        if (!mostSticky.Contains(i)) mostSticky.Add(i);
+    }
+    else
+    {
+        if (mostSticky.Contains(i)) mostSticky.Remove(i);
+
+        foreach (var can in cans[i])
+        {
+            if (!mostSticky.Contains(can) && can > i) mostSticky.Add(can);
+        }
+    }
+}
+
+Console.WriteLine(String.Join(" ", mostSticky.OrderBy(x => x)));
+Console.WriteLine();
+Console.WriteLine(mostSticky.Sum());
